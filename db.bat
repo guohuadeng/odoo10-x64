32c3ae6dce11f5ab5f20f08a6ea3eb699f3a55bc
Ivan Office <guohuadeng@hotmail.com>
2024-12-30 16:57:34 +0800

    update readme

--- db.bat
+++ db.bat
@@ -1,4 +1,4 @@
-title GreenOdoo10 x64 fast - www.sunpop.cn
+title GreenOdoo10 x64 fast - www.odooai.cn
 ping -n 2 127.0.0.1>nul
 SET PATH=%CD%\runtime\pgsql\bin;%CD%\runtime\python;%CD%\runtime\python\scripts;%CD%\runtime\win32\wkhtmltopdf;%CD%\runtime\win32\nodejs;%CD%\source;%PATH%
 %CD%\runtime\pgsql\bin\pg_ctl -D %CD%\runtime\pgsql\data -l %CD%\runtime\pgsql\logfile start
